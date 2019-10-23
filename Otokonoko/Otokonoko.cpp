bf36bd0e924946c6fbc026db9deb6df7f2699e4e
Naruto Mouri <Mouri_Naruto@outlook.com>
2019-10-24 00:49:14 +0800

    Add UefiPkg.

--- Otokonoko/Otokonoko.cpp
+++ Otokonoko/Otokonoko.cpp
@@ -43,8 +43,6 @@ extern "C" {
 #include <stdint.h>
 #endif
 
-
-
 /*
  * Otokonoko Platform (OP) Status
  */
@@ -59,27 +57,24 @@ EFI_BOOT_SERVICES* g_BootServices;
 
 EFI_GRAPHICS_OUTPUT_PROTOCOL* g_GraphicsOutputProtocol;
 
-
-
-
 EFI_STATUS UefiOutputString(
-	_In_ EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* Instance,
-	const wchar_t* String)
+    _In_ EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* Instance,
+    const wchar_t* String)
 {
-	return Instance->OutputString(Instance, const_cast<CHAR16*>(String));
+    return Instance->OutputString(Instance, const_cast<CHAR16*>(String));
 }
 
 
 [[noreturn]] void BugCheck()
 {
-	UefiOutputString(
-		g_SystemTable->ConOut,
-		L"\r\n"
-		L"\r\n"
-		L"BugCheck: Not implemented. [OPStatus = 0x000000BC]\r\n");
+    UefiOutputString(
+        g_SystemTable->ConOut,
+        L"\r\n"
+        L"\r\n"
+        L"BugCheck: Not implemented. [OPStatus = 0x000000BC]\r\n");
 
 
-	for (;;) {}
+    for (;;) {}
 }
 
 /**
@@ -87,60 +82,59 @@ EFI_STATUS UefiOutputString(
  *
  * @param ImageHandle The image handle of the UEFI Application.
  * @param SystemTable A pointer to the EFI System Table.
- * @return EFI_STATUS. If the function succeeds, the return value is 
+ * @return EFI_STATUS. If the function succeeds, the return value is
  *         EFI_SUCCESS.
  */
 EFI_STATUS EFIAPI UefiMain(
-	_In_ EFI_HANDLE        ImageHandle,
-	_In_ EFI_SYSTEM_TABLE* SystemTable)
+    _In_ EFI_HANDLE        ImageHandle,
+    _In_ EFI_SYSTEM_TABLE* SystemTable)
 {
-	g_ImageHandle = ImageHandle;
-	g_SystemTable = SystemTable;
-	g_BootServices = SystemTable->BootServices;
+    g_ImageHandle = ImageHandle;
+    g_SystemTable = SystemTable;
+    g_BootServices = SystemTable->BootServices;
 
-	// Disable UEFI watchdog timer
-	g_BootServices->SetWatchdogTimer(0, 0, 0, nullptr);
+    // Disable UEFI watchdog timer
+    g_BootServices->SetWatchdogTimer(0, 0, 0, nullptr);
 
-	EFI_STATUS Status = EFI_SUCCESS;
+    EFI_STATUS Status = EFI_SUCCESS;
 
-	EFI_GUID GraphicsOutputProtocolGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
-	Status = g_BootServices->LocateProtocol(
-		&GraphicsOutputProtocolGuid,
-		nullptr,
-		reinterpret_cast<void**>(&g_GraphicsOutputProtocol));
-	if (!EFI_ERROR(Status))
-	{
-		uint32_t* FrameBufferBase = reinterpret_cast<uint32_t*>(
-			g_GraphicsOutputProtocol->Mode->FrameBufferBase);
-		size_t FrameBufferSize = static_cast<size_t>(
-			g_GraphicsOutputProtocol->Mode->FrameBufferSize);		
+    Status = g_BootServices->LocateProtocol(
+        &gEfiGraphicsOutputProtocolGuid,
+        nullptr,
+        reinterpret_cast<void**>(&g_GraphicsOutputProtocol));
+    if (!EFI_ERROR(Status))
+    {
+        uint32_t* FrameBufferBase = reinterpret_cast<uint32_t*>(
+            g_GraphicsOutputProtocol->Mode->FrameBufferBase);
+        size_t FrameBufferSize = static_cast<size_t>(
+            g_GraphicsOutputProtocol->Mode->FrameBufferSize);
 
-		for (size_t i = 0; i < FrameBufferSize; ++i)
-		{
-			FrameBufferBase[i] = 0xFF000000;
-		}
-		
-	}
-	else
-	{
-		UefiOutputString(
-			g_SystemTable->ConOut,
-			L"Failed to locate the Graphics Output Protocol.\r\n");
-	}
+        for (size_t i = 0; i < FrameBufferSize; ++i)
+        {
+            FrameBufferBase[i] = 0xFF000000;
+        }
 
-	UefiOutputString(
-		g_SystemTable->ConOut, 
-		L"Otokonoko no Sekai Version 0.1 [Build 1]\r\n"
-		L"Copyright (C) 2019 Covariant Institute. All rights reserved.\r\n"
-		L"\r\n");
+    }
+    else
+    {
+        UefiOutputString(
+            g_SystemTable->ConOut,
+            L"Failed to locate the Graphics Output Protocol.\r\n");
+    }
 
-	UefiOutputString(
-		g_SystemTable->ConOut,
-		L"Hello World\r\n");
+    UefiOutputString(
+        g_SystemTable->ConOut,
+        L"Otokonoko no Sekai Version 0.1 [Build 1]\r\n"
+        L"Copyright (C) 2019 Covariant Institute. All rights reserved.\r\n"
+        L"\r\n");
 
-	BugCheck();
+    UefiOutputString(
+        g_SystemTable->ConOut,
+        L"Hello World\r\n");
 
-	return EFI_SUCCESS;
+    BugCheck();
+
+    return EFI_SUCCESS;
 }
 
 #pragma comment(linker, "/entry:UefiMain")
